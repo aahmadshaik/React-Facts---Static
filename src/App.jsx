843587313fbe94fc3b08609a7a6ec7fa83cbb8fc
aahmadshaik <shaikahmedaslam5676@gmail.com>
2024-12-24 13:03:42 +0530

    added some styles for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Header from "./components/Header.jsx";
 import Content from "./components/Content.jsx";
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <Content />
-    </>
+    </div>
   );
 }
 
